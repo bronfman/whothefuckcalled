cf441580c46160fd7be75d1b1f3924b1779abc1e
Randall Degges <rdegges@gmail.com>
2012-02-27 00:14:41 -0800

    Implementing some basic caching.

--- wtf/apps/core/ajax.py
+++ wtf/apps/core/ajax.py
@@ -1,4 +1,7 @@
+from re import sub
+
 from dajaxice.decorators import dajaxice_register
+from django.core.cache import cache
 from django.utils.simplejson import dumps
 from opencnam import Phone
 
@@ -6,5 +9,19 @@ from opencnam import Phone
 @dajaxice_register
 def lookup_cnam(request, phone_number):
     """Lookup caller ID information using opencnam's python API."""
-    phone = Phone(phone_number)
-    return dumps({'cnam': phone.cnam or ''})
+    phone_number = unicode(phone_number)
+    phone_number = sub(u'\D', '', phone_number)[-10:]
+
+    # Attempt to grab the caller ID name information from the cache.
+    cnam = cache.get(phone_number)
+
+    if not cnam:
+        cnam = Phone(phone_number).cnam
+
+        # If we got a result back from opencnam, then attempt to cache the name
+        # value for as long as possible (30 days with memcached). This way we
+        # can get faster responses and not hammer their free API.
+        if cnam:
+            cache.set(phone_number, cnam, 60 * 60 * 24 * 15)
+
+    return dumps({'cnam': cnam or ''})
