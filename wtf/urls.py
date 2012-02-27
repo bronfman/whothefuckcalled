3711e84582a2ff455918d2f32dcad4cfb7b1ce98
Randall Degges <rdegges@gmail.com>
2012-02-26 20:25:11 -0800

    Enabling the admin.

--- wtf/urls.py
+++ wtf/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,8 +12,8 @@ urlpatterns = patterns('',
     # url(r'^wtf/', include('wtf.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
