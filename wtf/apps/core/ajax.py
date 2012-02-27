342eb25cc046d457017fa0df9c6984abcb940d63
Randall Degges <rdegges@gmail.com>
2012-02-26 22:52:04 -0800

    debug

--- wtf/apps/core/ajax.py
+++ wtf/apps/core/ajax.py
@@ -6,5 +6,7 @@ from opencnam import Phone
 @dajaxice_register
 def lookup_cnam(request, phone_number):
     """Lookup caller ID information using opencnam's python API."""
+    print 'phone_number', phone_number
     phone = Phone(phone_number)
+    print 'phone_number.cnam', phone_number.cnam or ''
     return dumps({'cnam': phone.cnam or ''})
