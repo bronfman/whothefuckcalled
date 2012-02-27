1a1465e01a08d140e5d27ae2862ae9beef0097ec
Randall Degges <rdegges@gmail.com>
2012-02-26 23:40:39 -0800

    Removing debug code.

--- wtf/apps/core/ajax.py
+++ wtf/apps/core/ajax.py
@@ -6,8 +6,5 @@ from opencnam import Phone
 @dajaxice_register
 def lookup_cnam(request, phone_number):
     """Lookup caller ID information using opencnam's python API."""
-    #print 'phone_number', phone_number
     phone = Phone(phone_number)
-    #print 'phone_number.cnam', phone.cnam or ''
-    #print dumps({'cnam': phone.cnam or ''})
     return dumps({'cnam': phone.cnam or ''})
